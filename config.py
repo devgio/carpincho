b8d2ae7a2ab9d73c74ed2cebc8c1b24e1ab08649
devgio <36898483+devgio@users.noreply.github.com>
2021-03-15 20:00:33 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # GENERAL
-download_folder = r'C:\example_folder'
+download_folder = '/user/email_downloads'
 
 # MAILBOX SETTINGS
 from src.email_connectors.O365 import Mailbox as mailbox_class # Set the type of mailbox connector to use, so far the only available is O365
 mailbox_connection_parameters = ('credentials', 'credentials') # If the Azure credentials need to be updated, refer to https://pypi.org/project/O365/#authentication
-shared_mailbox = False # If not using a shared mailbox, set this to False
+shared_mailbox = False # If using a shared mailbox set to True
 inbox = ['Inbox']
 error_folder = ['Script_Error']
 unrecognized_folder = ['Unrecognized']
